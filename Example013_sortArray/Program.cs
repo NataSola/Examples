69e17e2e2de0d25ba350a88535f38c1894aaa4e0
NataSola <nsol2009@yandex.ru>
2021-11-02 23:34:31 +0300

    Add for

---
+++ Example013_sortArray/Program.cs
@@ -0,0 +1,25 @@
+﻿// Другие циклы
+
+string Method4(int count, string txt)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + txt;
+    }
+    return result;
+}
+
+string res = Method4(10, "00/");
+Console.WriteLine(res);
+
+// Цикл внутри цикла
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
